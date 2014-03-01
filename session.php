36b5b292e4f9e7c950dcf33f3cdeb41b861ddc9f
gh0st <shahidan@mycrypto.biz>
2014-03-01 10:31:43 +0800

    add function _construct()

--- session.php
+++ session.php
@@ -10,6 +10,27 @@ class Session {
 	 
 	private $db;
 	
+	
+	public function _construct() {
+		// instantiate new database object
+		$this->db = new Database;
+		
+		
+		// set handler to overide SESSION
+		session_set_save_handler(
+		    array($this, "_open"),
+		    array($this, "_close"),
+		    array($this, "_read"),
+		    array($this, "_write"),
+		    array($this, "_destroy"),
+		    array($this, "_gc")		
+		);
+		
+		// start the sesion
+		session_start();
+		
+	}
+	
 }
 
 ?>
